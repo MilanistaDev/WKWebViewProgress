c82a149a96741a1b1b25b6de20524ed819697b1b
Takuya Aso <milanista224@gmail.com>
2017-10-17 18:49:04 +0900

    implement WKWebView and Others.

--- WKWebViewProgress/ViewController.swift
+++ WKWebViewProgress/ViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wkWebView: WKWebView!
+    @IBOutlet weak var topLayoutConstraint: NSLayoutConstraint!
+    var progressView = UIProgressView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.navigationItem.title = "WKWebView"
+        // NavigationBar の下にUIprogressViewをBar形式で配置
+        self.progressView = UIProgressView(frame: CGRect(x: 0.0, y: (self.navigationController?.navigationBar.frame.size.height)! - 3.0, width: self.view.frame.size.width, height: 3.0))
+        self.progressView.progressViewStyle = .bar
+        self.navigationController?.navigationBar.addSubview(self.progressView)
+
+        if #available(iOS 11.0, *) {
+            self.topLayoutConstraint.constant = 0.0
+        } else {
+            self.topLayoutConstraint.constant = -64.0
+        }
+
+        self.wkWebView.navigationDelegate = self
+        self.wkWebView.uiDelegate = self
+
+        // KVO 監視
+        self.wkWebView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+
+        // URLリクエスト
+        let url = URL(string: "https://www.yumemi.co.jp")
+        self.wkWebView.load(URLRequest(url: url!, cachePolicy: .reloadRevalidatingCacheData, timeoutInterval: 10.0))
+    }
+
+    deinit {
+        self.wkWebView.removeObserver(self, forKeyPath: "estimatedProgress", context: nil)
+    }
+
+    /// 監視しているWKWebViewのestimatedProgressの値をUIProgressViewに反映
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+
+        if (keyPath == "estimatedProgress") {
+            // alphaを1にする(表示)
+            self.progressView.alpha = 1.0
+            // estimatedProgressが変更されたときにプログレスバーの値を変更
+            self.progressView.setProgress(Float(self.wkWebView.estimatedProgress), animated: true)
+
+            // estimatedProgressが1.0になったらアニメーションを使って非表示にしアニメーション完了時0.0をセットする
+            if (self.wkWebView.estimatedProgress >= 1.0) {
+                UIView.animate(withDuration: 0.3,
+                               delay: 0.3,
+                               options: [.curveEaseOut],
+                               animations: { [weak self] in
+                                self?.progressView.alpha = 0.0
+                    }, completion: {
+                        (finished : Bool) in
+                        self.progressView.setProgress(0.0, animated: false)
+                })
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+extension ViewController: WKNavigationDelegate {
+
+}
+
+extension ViewController: WKUIDelegate {
 
 }
 
